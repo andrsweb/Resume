2520ae3725934460f49bbd227bdee1a695f82bd3
proflinervk <proflinervk@gmail.com>
2022-10-05 08:34:06 +0300

    added header scrolled js and some design fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import './common/common'
 
 //sections
+import './sections/header/header'
 import './sections/hero/hero'
 import './sections/skills/skills'
 import './sections/portfolio/portfolio'
---
+++ src/js/sections/header/header.js
@@ -0,0 +1,22 @@
+document.addEventListener( 'DOMContentLoaded', () => {
+	'use strict'
+
+	headerScroll()
+} )
+
+	headerScroll = () => {
+		window.addEventListener( 'scroll', () => {
+			const scrollTop = window.scrollY
+			const header = document.querySelector( '.header' )
+
+			if ( scrollTop > 0 ) {
+				if ( ! header.classList.contains( 'scrolled' ) )
+					header.classList.add( 'scrolled' )
+
+			}   else {
+				if ( header.classList.contains( 'scrolled' ) )
+					header.classList.remove( 'scrolled' )
+			}
+		})
+	}
+
